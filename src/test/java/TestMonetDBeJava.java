2e054277035820ac94b379a5c692c396df481d13
Bernardo Mota <a77607@alunos.uminho.pt>
2020-09-28 15:11:25 +0200

    DriverManager issues

--- src/test/java/TestMonetDBeJava.java
+++ src/test/java/TestMonetDBeJava.java
@@ -1,6 +1,8 @@
 import nl.cwi.monetdb.monetdbe.MonetDriver;
 import nl.cwi.monetdb.monetdbe.MonetConnection;
 
+import org.duckdb.DuckDBDriver;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -8,7 +10,8 @@ import java.sql.SQLException;
 public class TestMonetDBeJava {
     static {
         try {
-            Class.forName("nl.cwi.monetdb.monetdbe.MonetDriver");
+            //Class.forName("nl.cwi.monetdb.monetdbe.MonetDriver");
+            Class.forName("org.duckdb.DuckDBDriver");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
@@ -17,7 +20,8 @@ public class TestMonetDBeJava {
     public static void main (String[] args) {
         try {
             System.out.println(DriverManager.getDrivers().nextElement().toString());
-            Connection conn = DriverManager.getConnection("jdbc:monetdb://localhost/test");
+            //Connection conn = DriverManager.getConnection("jdbc:monetdb://localhost/test");
+            Connection conn = DriverManager.getConnection("jdbc:duckdb://localhost/test");
         } catch (SQLException e) {
             e.printStackTrace();
         }
