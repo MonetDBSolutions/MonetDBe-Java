125e9381d09c829f0ed6d65d02fae0f5f2af46c6
Bernardo Mota <a77607@alunos.uminho.pt>
2020-09-28 14:15:43 +0200

    DriverManager issues

--- test/TestMonetDBeJava.java
+++ test/TestMonetDBeJava.java
@@ -5,20 +5,28 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class TestMonetDBeJava {
-    static {
-        try {
-            Class.forName("MonetDriver");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
+    public TestMonetDBeJava() {
+        test();
     }
 
-    public static void main (String[] args) {
+    public void test() {
         try {
             System.out.println(DriverManager.getDrivers().nextElement().toString());
             Connection conn = DriverManager.getConnection("jdbc:monetdb://localhost/test");
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    static {
+        try {
+            Class.forName("nl.cwi.monetdb.monetdbe.MonetDriver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main (String[] args) {
+        TestMonetDBeJava t = new TestMonetDBeJava();
     }
 }
