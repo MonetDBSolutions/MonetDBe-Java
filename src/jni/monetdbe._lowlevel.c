6a07f04954e5bcfa7d08dacb8103ba832d881058
Bernardo Mota <a77607@alunos.uminho.pt>
2020-09-28 11:45:35 +0200

    C Wrapper fixes

--- src/jni/monetdbe._lowlevel.c
+++ src/jni/monetdbe._lowlevel.c
@@ -5,14 +5,14 @@
 JNIEXPORT jint JNICALL Java_nl_cwi_monetdb_monetdbe_MonetNative_monetdbe_1open (JNIEnv * env, jclass self, jobject j_dbhdl, jstring j_url, jobject j_opts) {
   //convert and access resources
   monetdbe_database *dbhdl = (*env)->GetDirectBufferAddress(env,j_dbhdl);
-  char *url = (*env)->GetStringUTFChars(env, j_url, NULL);
+  char *url = env->GetStringUTFChars(env, j_url, NULL);
   monetdbe_options *opts = (*env)->GetDirectBufferAddress(env,j_opts);
 
   //call monetdbe_open
   int result = monetdbe_open(*dbhdl,*url,*opts);
 
   //release resources
-  (*env)->ReleaseStringUTFChars(env, j_url, url);
+  env->ReleaseStringUTFChars(env, j_url, url);
   return result;
 }
 
