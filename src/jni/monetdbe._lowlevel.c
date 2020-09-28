f7a4001ffcff6527016e804d1c919987d5efe5e4
Bernardo Mota <a77607@alunos.uminho.pt>
2020-09-28 11:41:57 +0200

    C Wrapper fixes

--- src/jni/monetdbe._lowlevel.c
+++ src/jni/monetdbe._lowlevel.c
@@ -2,14 +2,14 @@
 #include "nl_cwi_monetdb_monetdbe_MonetNative.h"
 #include "monetdbe.c"
 
-JNIEXPORT jint JNICALL Java_nl_cwi_monetdb_monetdbe_MonetNative_monetdbe_1open (JNIEnv * env, jclass, jobject j_dbhdl, jstring j_url, jobject j_opts) {
+JNIEXPORT jint JNICALL Java_nl_cwi_monetdb_monetdbe_MonetNative_monetdbe_1open (JNIEnv * env, jclass self, jobject j_dbhdl, jstring j_url, jobject j_opts) {
   //convert and access resources
-  monetdbe_database *dbhdl = (*env)->GetDirectBufferAddress(j_dbhdl);
+  monetdbe_database *dbhdl = (*env)->GetDirectBufferAddress(env,j_dbhdl);
   char *url = (*env)->GetStringUTFChars(env, j_url, NULL);
-  monetdbe_options *opts = (*env)->GetDirectBufferAddress(j_opts);
+  monetdbe_options *opts = (*env)->GetDirectBufferAddress(env,j_opts);
 
   //call monetdbe_open
-  int result = monetdbe_open(dbhdl,url,opts);
+  int result = monetdbe_open(*dbhdl,*url,*opts);
 
   //release resources
   (*env)->ReleaseStringUTFChars(env, j_url, url);
@@ -21,8 +21,8 @@ JNIEXPORT jint JNICALL Java_nl_cwi_monetdb_monetdbe_MonetNative_monetdbe_1open (
  * Method:    monetdbe_close
  * Signature: (Ljava/nio/ByteBuffer;)I
  */
-JNIEXPORT jint JNICALL Java_nl_cwi_monetdb_monetdbe_MonetNative_monetdbe_1close (JNIEnv * env, jclass, jobject j_dbhdl) {
-  monetdbe_database *dbhdl = (*env)->GetDirectBufferAddress(j_dbhdl);
-  int result = monetdbe_open(dbhdl,url,opts);
+JNIEXPORT jint JNICALL Java_nl_cwi_monetdb_monetdbe_MonetNative_monetdbe_1close (JNIEnv * env, jclass self, jobject j_dbhdl) {
+  monetdbe_database *dbhdl = (*env)->GetDirectBufferAddress(env,j_dbhdl);
+  int result = monetdbe_open(*dbhdl,*url,*opts);
   return result;
 }
