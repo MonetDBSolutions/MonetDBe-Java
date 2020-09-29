a640df3e26a5e821c2ad20a7101ba52db5a8bf4d
Bernardo Mota <a77607@alunos.uminho.pt>
2020-09-29 12:30:37 +0200

    Added monetdbe_error

--- src/jni/monetdbe._lowlevel.c
+++ src/jni/monetdbe._lowlevel.c
@@ -2,31 +2,28 @@
 #include "nl_cwi_monetdb_monetdbe_MonetNative.h"
 #include "monetdbe.c"
 
-JNIEXPORT jint JNICALL Java_nl_cwi_monetdb_monetdbe_MonetNative_monetdbe_1open (JNIEnv* env, jclass self, jobject j_dbhdl, jstring j_url, jobject j_opts) {
+JNIEXPORT jint JNICALL Java_nl_cwi_monetdb_monetdbe_MonetNative_monetdbe_1open (JNIEnv* env, jclass self, jobject j_db, jstring j_url, jobject j_opts) {
   //convert and access resources
-  monetdbe_database* dbhdl = (*env)->GetDirectBufferAddress(env,j_dbhdl);
-  //Has to be const
-  const char* const_url = (*env)->GetStringUTFChars(env,j_url,NULL);
-  char* url = malloc(strlen(const_url));
-  strcpy(url,const_url);
-  //char* url = env->GetStringUTFChars(env, j_url, NULL);
+  monetdbe_database* db = (*env)->GetDirectBufferAddress(env,j_db);
+  char* const_url = (char*) (*env)->GetStringUTFChars(env,j_url,NULL);
   monetdbe_options* opts = (*env)->GetDirectBufferAddress(env,j_opts);
 
   //call monetdbe_open
-  int result = monetdbe_open(dbhdl,url,opts);
+  int result = monetdbe_open(db,url,opts);
 
   //release resources
   (*env)->ReleaseStringUTFChars(env, j_url, url);
   return result;
 }
 
-/*
- * Class:     nl_cwi_monetdb_monetdbe_MonetNative
- * Method:    monetdbe_close
- * Signature: (Ljava/nio/ByteBuffer;)I
- */
-JNIEXPORT jint JNICALL Java_nl_cwi_monetdb_monetdbe_MonetNative_monetdbe_1close (JNIEnv * env, jclass self, jobject j_dbhdl) {
-  monetdbe_database* dbhdl = (*env)->GetDirectBufferAddress(env,j_dbhdl);
-  int result = monetdbe_close(dbhdl);
+JNIEXPORT jint JNICALL Java_nl_cwi_monetdb_monetdbe_MonetNative_monetdbe_1close (JNIEnv * env, jclass self, jobject j_db) {
+  monetdbe_database* db = (*env)->GetDirectBufferAddress(env,j_db);
+  int result = monetdbe_close(db);
   return result;
+}
+
+JNIEXPORT jstring JNICALL Java_nl_cwi_monetdb_monetdbe_MonetNative_monetdbe_1error (JNIEnv * env, jclass self, jobject j_db) {
+  monetdbe_database* db = (*env)->GetDirectBufferAddress(env,j_db);
+  const char* result = (const char*) monetdbe_error(db);
+  return (*env)->NewStringUTF(env,result);
 }
