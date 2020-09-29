1c0b5b4eda7685cdb9fd9ebf75599ea6b38bb95d
Bernardo Mota <a77607@alunos.uminho.pt>
2020-09-29 13:07:29 +0200

    Changed jstring to jbytearray

--- src/jni/monetdbe._lowlevel.c
+++ src/jni/monetdbe._lowlevel.c
@@ -2,13 +2,28 @@
 #include "nl_cwi_monetdb_monetdbe_MonetNative.h"
 #include "monetdbe.c"
 
-JNIEXPORT jint JNICALL Java_nl_cwi_monetdb_monetdbe_MonetNative_monetdbe_1open (JNIEnv* env, jclass self, jobject j_db, jstring j_url, jobject j_opts) {
+char* byte_array_to_string(JNIEnv *env, jbyteArray array_j) {
+	int len = (*env)->GetArrayLength(env,array_j);
+	char* string = malloc(len);
+
+	jbyte* bytes = (jbyte*) (*env)->GetByteArrayElements(ba_j, NULL);
+
+	for (idx_t i = 0; i < len; i++) {
+		string[i] = bytes[i];
+	}
+	(*env)->ReleaseByteArrayElements(array_j, bytes, 0);
+
+	return ret;
+}
+
+JNIEXPORT jint JNICALL Java_nl_cwi_monetdb_monetdbe_MonetNative_monetdbe_1open (JNIEnv* env, jclass self, jobject j_db, jbyteArray j_url, jobject j_opts) {
   //convert and access resources
   monetdbe_database* db = (*env)->GetDirectBufferAddress(env,j_db);
   //char* url = (char*) (*env)->GetStringUTFChars(env,j_url,NULL);
-  const char* const_url = (*env)->GetStringUTFChars(env,j_url,0);
-  char* url = malloc(strlen(const_url));
-  strcpy(url,const_url);
+  //const char* const_url = (*env)->GetStringUTFChars(env,j_url,0);
+  //char* url = malloc(strlen(const_url));
+  //strcpy(url,const_url);
+  char* url = byte_array_to_string(env,j_url)
   monetdbe_options* opts = (*env)->GetDirectBufferAddress(env,j_opts);
 
   //call monetdbe_open
