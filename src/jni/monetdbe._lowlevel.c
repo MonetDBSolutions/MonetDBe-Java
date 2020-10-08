494215f85a3e9f9da4f02801946c8c55664be9eb
Bernardo Mota <a77607@alunos.uminho.pt>
2020-10-08 11:02:15 +0200

    Changed jstring to jbytearray

--- src/jni/monetdbe._lowlevel.c
+++ src/jni/monetdbe._lowlevel.c
@@ -53,6 +53,6 @@ JNIEXPORT jstring JNICALL Java_nl_cwi_monetdb_monetdbe_MonetNative_monetdbe_1err
   char *buf = (char*)malloc(10);
   strcpy(buf, "123456789");
 
-  jstring result_string = (*env)->NewStringUTF(buf,r);
+  jstring result_string = (*env)->NewStringUTF((const char*) buf,r);
   return result_string;
 }
