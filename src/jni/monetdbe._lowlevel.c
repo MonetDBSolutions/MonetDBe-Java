4b7caf80e3526169cdbad19d0d884fff5ffb0200
Bernardo Mota <a77607@alunos.uminho.pt>
2020-10-08 13:12:55 +0200

    Changed jstring to jbytearray

--- src/jni/monetdbe._lowlevel.c
+++ src/jni/monetdbe._lowlevel.c
@@ -28,27 +28,26 @@ jbyteArray string_to_byte_array(JNIEnv *env, char* string) {
     return array;
 }
 
-
-JNIEXPORT jint JNICALL Java_nl_cwi_monetdb_monetdbe_MonetNative_monetdbe_1open (JNIEnv* env, jclass self, jobject j_db, jbyteArray j_url, jobject j_opts) {
+JNIEXPORT jint JNICALL Java_nl_cwi_monetdb_monetdbe_MonetNative_monetdbe_1open (JNIEnv* env, jclass self, jobject j_db, jstring j_url, jobject j_opts) {
   monetdbe_database* db = (*env)->GetDirectBufferAddress(env,j_db);
-  /*char* url = (char*) (*env)->GetStringUTFChars(env,j_url,NULL);
-  printf("%s", url);
-  fflush(stdout);*/
-  //const char* const_url = (*env)->GetStringUTFChars(env,j_url,0);
-  //char* url = malloc(strlen(const_url));
-  //strcpy(url,const_url);
-  char* url = byte_array_to_string(env,j_url);
+  char* url = (char*) (*env)->GetStringUTFChars(env,j_url,NULL);
   printf("%s", url);
   fflush(stdout);
   monetdbe_options* opts = (*env)->GetDirectBufferAddress(env,j_opts);
 
-  //call monetdbe_open
   int result = monetdbe_open(db,url,opts);
-
-  //release resources
   (*env)->ReleaseStringUTFChars(env, j_url, url);
   return result;
 }
+
+/*JNIEXPORT jint JNICALL Java_nl_cwi_monetdb_monetdbe_MonetNative_monetdbe_1open (JNIEnv* env, jclass self, jobject j_db, jbyteArray j_url, jobject j_opts) {
+  monetdbe_database* db = (*env)->GetDirectBufferAddress(env,j_db);
+  char* url = byte_array_to_string(env,j_url);
+  monetdbe_options* opts = (*env)->GetDirectBufferAddress(env,j_opts);
+  int result = monetdbe_open(db,url,opts);
+  (*env)->ReleaseStringUTFChars(env, j_url, url);
+  return result;
+}*/
 
 JNIEXPORT jint JNICALL Java_nl_cwi_monetdb_monetdbe_MonetNative_monetdbe_1close (JNIEnv * env, jclass self, jobject j_db) {
   monetdbe_database* db = (*env)->GetDirectBufferAddress(env,j_db);
