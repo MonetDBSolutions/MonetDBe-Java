5ef5ddbc3a2530cd42e18f650004cc3f3f337dd5
Bernardo Mota <a77607@alunos.uminho.pt>
2020-10-08 11:58:50 +0200

    Changed jstring to jbytearray

--- src/jni/monetdbe._lowlevel.c
+++ src/jni/monetdbe._lowlevel.c
@@ -78,7 +78,7 @@ JNIEXPORT jint JNICALL Java_nl_cwi_monetdb_monetdbe_MonetNative_monetdbe_1close 
 JNIEXPORT jstring JNICALL Java_nl_cwi_monetdb_monetdbe_MonetNative_monetdbe_1error (JNIEnv * env, jclass self, jobject j_db) {
   monetdbe_database* db = (*env)->GetDirectBufferAddress(env,j_db);
   char* result = monetdbe_error(db);
-  printf("%d\n",strlen(result));
+  printf("%d %s\n",strlen(result),result);
   //printf("%s\n", result);
   //char* r = (char*) malloc(strlen(result+1));
   //strcpy(r,result);
