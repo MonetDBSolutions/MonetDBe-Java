a0315495d4e25b3f5c433edb021defa6d3b5e5e0
Bernardo Mota <a77607@alunos.uminho.pt>
2020-10-11 15:14:35 +0200

    Starting statement implementation

--- src/main/java/nl/cwi/monetdb/monetdbe/MonetConnection.java
+++ src/main/java/nl/cwi/monetdb/monetdbe/MonetConnection.java
@@ -14,20 +14,18 @@ public class MonetConnection implements Connection {
     private ByteBuffer connection;
     private boolean autoCommit = true;
 
-
     MonetConnection(final Properties props) throws SQLException, IllegalArgumentException {
         connection = MonetNative.monetdbe_open("/home/bernardo/MonetDB-Jun2020/db-farm/test");
-        System.out.println("Database pointer: " + connection);
         String error = MonetNative.monetdbe_error(connection);
         System.out.println("Error: " + error);
-        //System.out.println("Error: " + new String(error,StandardCharsets.UTF_8));
-        int result = MonetNative.monetdbe_close(connection);
-        System.out.println("Close result: " + result);
     }
 
     @Override
     public Statement createStatement() throws SQLException {
-        return null;
+        if(isClosed()) {
+            throw new SQLException("Connection is closed.");
+        }
+        return new MonetStatement(this);
     }
 
     @Override
@@ -67,12 +65,16 @@ public class MonetConnection implements Connection {
 
     @Override
     public void close() throws SQLException {
-
+        if(isClosed()) {
+            throw new SQLException("Connection already closed.");
+        }
+        MonetNative.monetdbe_close(connection);
+        connection = null;
     }
 
     @Override
     public boolean isClosed() throws SQLException {
-        return false;
+        return connection == null;
     }
 
     @Override
--- src/main/java/nl/cwi/monetdb/monetdbe/MonetNative.java
+++ src/main/java/nl/cwi/monetdb/monetdbe/MonetNative.java
@@ -9,5 +9,6 @@ public class MonetNative {
 
     protected static native ByteBuffer monetdbe_open(String dbdir);
     protected static native int monetdbe_close(ByteBuffer db);
+    protected static native ByteBuffer monetdbe_execute(ByteBuffer statement);
     protected static native String monetdbe_error(ByteBuffer db);
 }
