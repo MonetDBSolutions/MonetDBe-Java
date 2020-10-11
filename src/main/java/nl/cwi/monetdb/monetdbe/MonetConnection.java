e1c496fff4d6cb8e4d73bf69b9fcd5476c02a2a9
Bernardo Mota <a77607@alunos.uminho.pt>
2020-10-11 14:32:32 +0200

    Trying DirectByteBuffer for C pointers

--- src/main/java/nl/cwi/monetdb/monetdbe/MonetConnection.java
+++ src/main/java/nl/cwi/monetdb/monetdbe/MonetConnection.java
@@ -16,8 +16,7 @@ public class MonetConnection implements Connection {
 
 
     MonetConnection(final Properties props) throws SQLException, IllegalArgumentException {
-        //jdbc:monetdb://localhost/test
-        connection = MonetNative.monetdbe_open("monetdb://localhost/test");
+        connection = MonetNative.monetdbe_open("~/MonetDB-Jun2020/db-farm/test");
         System.out.println("Database pointer: " + connection);
         //byte[] error = MonetNative.monetdbe_error(database);
         //System.out.println("Error: " + new String(error,StandardCharsets.UTF_8));
