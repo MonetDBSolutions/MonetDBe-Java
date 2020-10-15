5e63664eb3f1b14e821b3b9cc1b628f71d7606a2
Bernardo Mota <a77607@alunos.uminho.pt>
2020-10-15 17:48:18 +0200

    Added simple conversions to ByteBuffer for all types

--- src/jni/monetdbe._lowlevel.c
+++ src/jni/monetdbe._lowlevel.c
@@ -7,7 +7,7 @@
 void addColumn(JNIEnv *env, jobjectArray j_data_columns, void* data, int size, int index) {
     jobject j_data = (*env)->NewDirectByteBuffer(env,data,size);
     //jobject j_data = (*env)->NewDirectByteBuffer(env,col->data,8*col->count);
-    for(int i = 0; i<size;i++) {
+    for(int i = 0; i<4;i++) {
         if (size/4 == 32) {
             long* data_i = (long*) data;
             printf("%li\n",data_i[i]);
