c23d47d6b7257affb7ecbc20969ed62cd6c6bfe3
Bernardo Mota <a77607@alunos.uminho.pt>
2020-10-15 13:12:25 +0200

    Trying to fix new errors

--- src/jni/monetdbe._lowlevel.c
+++ src/jni/monetdbe._lowlevel.c
@@ -42,8 +42,8 @@ JNIEXPORT jobject JNICALL Java_nl_cwi_monetdb_monetdbe_MonetNative_monetdbe_1ope
   opts->sessiontimeout = j_sessiontimeout;
   opts->nr_threads = j_nr_threads;
 
-  char* url = (char*) (*env)->GetStringUTFChars(env,j_url,NULL);
-  int result = monetdbe_open(db,NULL,opts);
+  char* url = (char*) (*env)->GetStringUTFChars(env,j_url,NULL)
+  int result = monetdbe_open(db,url,NULL);
   (*env)->ReleaseStringUTFChars(env, j_url, url);
 
   if (result != 0) {
