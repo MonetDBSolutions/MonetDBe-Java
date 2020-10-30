5a37b88991375a0b10550f8cabda545bfb4cf5b5
Bernardo Mota <a77607@alunos.uminho.pt>
2020-10-30 15:51:22 +0100

    Fix

--- src/test/java/TestMonetDBeJava.java
+++ src/test/java/TestMonetDBeJava.java
@@ -19,7 +19,7 @@ public class TestMonetDBeJava {
             MonetStatement s = (MonetStatement) c.createStatement();
 
             System.out.println("Create table");
-            //s.execute("CREATE TABLE a(id int, name string);");
+            s.execute("CREATE TABLE a(id int, name string);");
             //s.execute("INSERT INTO a VALUES (1,'a'), (2,'b'), (3,'c');");
             //s.execute("CREATE TABLE a (b bool);");
             //s.execute("INSERT INTO a VALUES (TRUE), (TRUE), (FALSE);");
@@ -28,13 +28,13 @@ public class TestMonetDBeJava {
 
             s.execute("CREATE TABLE a (b boolean, s smallint, i int, l bigint, f real, d double, st string, da date, t time, ts timestamp);");
 
+            System.out.println("Insert into");
             s.execute("INSERT INTO a VALUES " +
                     "(true, 2, 3, 5, 1.0, 1.66,'hey1',str_to_date('23-09-1987', '%d-%m-%Y'),str_to_time('11:40:30', '%H:%M:%S'),str_to_timestamp('23-09-1987 11:40', '%d-%m-%Y %H:%M')), " +
                     "(true, 4, 6, 10, 2.5, 3.643,'hey2',str_to_date('23-09-1990', '%d-%m-%Y'),str_to_time('11:40:35', '%H:%M:%S'),str_to_timestamp('23-09-1990 11:40', '%d-%m-%Y %H:%M')), " +
                     "(false, 8, 12, 20, 25.25, 372.325,'hey3',str_to_date('24-09-2007', '%d-%m-%Y'),str_to_time('12:01:59', '%H:%M:%S'),str_to_timestamp('24-09-2007 12:01', '%d-%m-%Y %H:%M')), " +
                     "(false, 16, 24, 40, 255.255, 2434.432,'hey4',str_to_date('24-12-2007', '%d-%m-%Y'),str_to_time('14:11:29', '%H:%M:%S'),str_to_timestamp('24-12-2007 14:11', '%d-%m-%Y %H:%M'));");
 
-            System.out.println("Insert update count: " + s.getUpdateCount());
             s.execute("SELECT * FROM a;");
             MonetResultSet rs = (MonetResultSet) s.getResultSet();
             System.out.println("Select resultSet: ");
