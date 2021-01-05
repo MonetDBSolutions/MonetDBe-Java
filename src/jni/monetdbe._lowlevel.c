6913c4f51cca8d1303ffecbd59cd46f4710dda8e
Bernardo Mota <a77607@alunos.uminho.pt>
2021-01-05 12:55:28 +0000

    Fix

--- src/jni/monetdbe._lowlevel.c
+++ src/jni/monetdbe._lowlevel.c
@@ -123,8 +123,8 @@ JNIEXPORT jobject JNICALL Java_nl_cwi_monetdb_monetdbe_MonetNative_monetdbe_1ope
   opts->querytimeout = (int) j_querytimeout;
   opts->sessiontimeout = (int) j_sessiontimeout;
   opts->nr_threads = (int) j_nr_threads;
-  opts->remote = NULL;
-  opts->mapi_server = NULL;
+  //opts->remote = NULL;
+  //opts->mapi_server = NULL;
 
   char* url = (char*) (*env)->GetStringUTFChars(env,j_url,NULL);
   int result;
