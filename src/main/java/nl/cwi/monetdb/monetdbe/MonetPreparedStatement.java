590dfec9403552320be7c4c2e341206bba7b837c
Bernardo Mota <a77607@alunos.uminho.pt>
2021-01-14 14:45:48 +0000

    Missing methods/TODOs in PreparedStatement

--- src/main/java/nl/cwi/monetdb/monetdbe/MonetPreparedStatement.java
+++ src/main/java/nl/cwi/monetdb/monetdbe/MonetPreparedStatement.java
@@ -12,13 +12,16 @@ import java.time.LocalTime;
 import java.time.ZoneId;
 import java.time.temporal.TemporalField;
 import java.time.temporal.TemporalUnit;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
 //TODO Check if the statement is closed before doing actions which depend on it
-//TODO Add check to verify Statement is not closed and parameter number is valid before bind/executes
 public class MonetPreparedStatement extends MonetStatement implements PreparedStatement {
     private ByteBuffer statementNative;
     protected int nParams;
+    private Object[] parameters;
+    private List<Object[]> parametersBatch = null;
 
     public MonetPreparedStatement(MonetConnection conn, String sql) {
         super(conn);
@@ -27,15 +30,16 @@ public class MonetPreparedStatement extends MonetStatement implements PreparedSt
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
         //nParams is set within monetdbe_prepare
         this.statementNative = MonetNative.monetdbe_prepare(conn.getDbNative(),sql, this);
+        if (nParams > 0) {
+            this.parameters = new Object[nParams];
+        }
     }
 
     //Executes
     @Override
     public boolean execute() throws SQLException {
-        //TODO Should I test if all parameters are set? Or leave the server to respond with an error?
         this.resultSet = MonetNative.monetdbe_execute(statementNative,this, false);
         if (this.resultSet!=null) {
             return true;
@@ -44,7 +48,7 @@ public class MonetPreparedStatement extends MonetStatement implements PreparedSt
             return false;
         }
         else {
-            //TODO Improve this (happens when an error message is sent by the server)
+            //TODO Improve this (happens when an error message is sent by the server, p.e. not all parameters are set)
             throw new SQLException("Server error");
         }
     }
@@ -83,8 +87,51 @@ public class MonetPreparedStatement extends MonetStatement implements PreparedSt
 
     @Override
     public void addBatch() throws SQLException {
-        //TODO
         //This allows us to add multiple "versions" of the same query, using different parameters
+        if (parametersBatch == null) {
+            parametersBatch = new ArrayList<>();
+        }
+        parametersBatch.add(parameters);
+        parameters = new Object[nParams];
+    }
+
+    //Overrides Statement's implementation, which batches different queries instead of different parameters for same query
+    //TODO Test
+    @Override
+    public int[] executeBatch() throws SQLException {
+        if (parametersBatch == null || parametersBatch.isEmpty()) {
+            return new int[0];
+        }
+        int[] counts = new int[parametersBatch.size()];
+        int count = -1;
+        Object[] cur_batch;
+        Object x;
+
+        for (int i = 0; i < parametersBatch.size(); i++) {
+            //Get batch of parameters
+            cur_batch = parametersBatch.get(i);
+
+            for (int j = 0; j < nParams; j++) {
+                //Set each parameter in current batch
+                x = cur_batch[j];
+                setObject(j+1,x);
+            }
+
+            try {
+                count = executeUpdate();
+            } catch (SQLException e) {
+                //Query returned a resultSet, throw BatchUpdateException
+                throw new BatchUpdateException();
+            }
+            if (count >= 0) {
+                counts[i] = count;
+            }
+            else {
+                counts[i] = Statement.SUCCESS_NO_INFO;
+            }
+        }
+        clearBatch();
+        return counts;
     }
 
     @Override
@@ -121,6 +168,7 @@ public class MonetPreparedStatement extends MonetStatement implements PreparedSt
     }
 
     //Set objects
+    //TODO setObject conversions
     @Override
     public void setObject(int parameterIndex, Object x, int targetSqlType, int scaleOrLength) throws SQLException {
         int monetType = MonetTypes.getMonetTypeInt(targetSqlType);
@@ -183,69 +231,82 @@ public class MonetPreparedStatement extends MonetStatement implements PreparedSt
     @Override
     public void setNull(int parameterIndex, int sqlType) throws SQLException {
         MonetNative.monetdbe_bind(statementNative,null,MonetTypes.getMonetTypeInt(sqlType),parameterIndex);
+        parameters[parameterIndex-1] = null;
     }
 
     @Override
     public void setBoolean(int parameterIndex, boolean x) throws SQLException {
         MonetNative.monetdbe_bind(statementNative,x,0,parameterIndex);
+        parameters[parameterIndex-1] = x;
     }
 
+    //TODO Test
     @Override
     public void setByte(int parameterIndex, byte x) throws SQLException {
-        //TODO Test
         MonetNative.monetdbe_bind(statementNative,x,1,parameterIndex);
+        parameters[parameterIndex-1] = x;
     }
 
     @Override
     public void setShort(int parameterIndex, short x) throws SQLException {
         MonetNative.monetdbe_bind(statementNative,x,2,parameterIndex);
+        parameters[parameterIndex-1] = x;
     }
 
     @Override
     public void setInt(int parameterIndex, int x) throws SQLException {
         MonetNative.monetdbe_bind(statementNative,x,3,parameterIndex);
+        parameters[parameterIndex-1] = x;
     }
 
     @Override
     public void setLong(int parameterIndex, long x) throws SQLException {
         MonetNative.monetdbe_bind(statementNative,x,4,parameterIndex);
+        parameters[parameterIndex-1] = x;
     }
 
     @Override
     public void setFloat(int parameterIndex, float x) throws SQLException {
         MonetNative.monetdbe_bind(statementNative,x,7,parameterIndex);
+        parameters[parameterIndex-1] = x;
     }
 
     @Override
     public void setDouble(int parameterIndex, double x) throws SQLException {
         MonetNative.monetdbe_bind(statementNative,x,8,parameterIndex);
+        parameters[parameterIndex-1] = x;
     }
 
+    //TODO BIG DECIMAL
     @Override
     public void setBigDecimal(int parameterIndex, BigDecimal x) throws SQLException {
-        //TODO BIG DECIMAL
+
     }
 
     @Override
     public void setString(int parameterIndex, String x) throws SQLException {
         MonetNative.monetdbe_bind(statementNative,x,9,parameterIndex);
+        parameters[parameterIndex-1] = x;
     }
 
+    //TODO Bytes - is there a type which translates to bytes?
     @Override
     public void setBytes(int parameterIndex, byte[] x) throws SQLException {
-        //TODO Bytes
+
     }
 
     @Override
     public void setDate(int parameterIndex, Date x) throws SQLException {
         LocalDate localDate = x.toLocalDate();
         MonetNative.monetdbe_bind_date(statementNative,parameterIndex,localDate.getYear(),localDate.getMonthValue(),localDate.getDayOfMonth());
+        parameters[parameterIndex-1] = x;
     }
 
     @Override
     public void setTime(int parameterIndex, Time x) throws SQLException {
         LocalTime localTime = x.toLocalTime();
         MonetNative.monetdbe_bind_time(statementNative,parameterIndex,localTime.getHour(),localTime.getMinute(),localTime.getSecond(),localTime.getNano()*1000);
+        parameters[parameterIndex-1] = x;
     }
 
     @Override
@@ -253,92 +314,36 @@ public class MonetPreparedStatement extends MonetStatement implements PreparedSt
         LocalDateTime localDateTime = x.toLocalDateTime();
         //TODO ms
         MonetNative.monetdbe_bind_timestamp(statementNative,parameterIndex,localDateTime.getYear(),localDateTime.getMonthValue(),localDateTime.getDayOfMonth(),localDateTime.getHour(),localDateTime.getMinute(),localDateTime.getSecond(),0);
+        parameters[parameterIndex-1] = x;
     }
 
+    //TODO Calendar
     @Override
     public void setDate(int parameterIndex, Date x, Calendar cal) throws SQLException {
-        //TODO Calendar
+
     }
 
+    //TODO Calendar
     @Override
     public void setTime(int parameterIndex, Time x, Calendar cal) throws SQLException {
-        //TODO Calendar
+
     }
 
+    //TODO Calendar
     @Override
     public void setTimestamp(int parameterIndex, Timestamp x, Calendar cal) throws SQLException {
-        //TODO Calendar
+
     }
 
     @Override
     public void setURL(int parameterIndex, URL x) throws SQLException {
         setString(parameterIndex,x.toString());
+        parameters[parameterIndex-1] = x;
     }
 
+    //TODO BLOB
     @Override
     public void setBlob(int parameterIndex, Blob x) throws SQLException {
-        //TODO BLOB
-    }
-
-    @Override
-    public void setNull(int parameterIndex, int sqlType, String typeName) throws SQLException {
-        //TODO Ref and UDFs
-    }
-
-    //Set other objects (Ref, Clob, Array, NString, NClob, XML)
-    //TODO Other objects
-    @Override
-    public void setRef(int parameterIndex, Ref x) throws SQLException {
-
-    }
-
-    @Override
-    public void setClob(int parameterIndex, Clob x) throws SQLException {
-
-    }
-
-    @Override
-    public void setArray(int parameterIndex, Array x) throws SQLException {
-
-    }
-
-    @Override
-    public void setRowId(int parameterIndex, RowId x) throws SQLException {
-
-    }
-
-    @Override
-    public void setNString(int parameterIndex, String value) throws SQLException {
-
-    }
-
-    @Override
-    public void setNClob(int parameterIndex, NClob value) throws SQLException {
-
-    }
-
-    @Override
-    public void setClob(int parameterIndex, Reader reader, long length) throws SQLException {
-
-    }
-
-    @Override
-    public void setBlob(int parameterIndex, InputStream inputStream, long length) throws SQLException {
-
-    }
-
-    @Override
-    public void setNClob(int parameterIndex, Reader reader, long length) throws SQLException {
-
-    }
-
-    @Override
-    public void setSQLXML(int parameterIndex, SQLXML xmlObject) throws SQLException {
-
-    }
-
-    @Override
-    public void setClob(int parameterIndex, Reader reader) throws SQLException {
 
     }
 
@@ -348,8 +353,70 @@ public class MonetPreparedStatement extends MonetStatement implements PreparedSt
     }
 
     @Override
-    public void setNClob(int parameterIndex, Reader reader) throws SQLException {
+    public void setBlob(int parameterIndex, InputStream inputStream, long length) throws SQLException {
 
+    }
+
+    //TODO CLOB
+    @Override
+    public void setClob(int parameterIndex, Clob x) throws SQLException {
+
+    }
+
+    @Override
+    public void setClob(int parameterIndex, Reader reader) throws SQLException {
+
+    }
+
+    @Override
+    public void setClob(int parameterIndex, Reader reader, long length) throws SQLException {
+
+    }
+    
+    @Override
+    public void setNull(int parameterIndex, int sqlType, String typeName) throws SQLException {
+        //TODO Ref and UDFs?
+    }
+
+    //Set other objects (Ref, Array, NString, NClob, XML)
+    @Override
+    public void setNString(int parameterIndex, String value) throws SQLException {
+        setString(parameterIndex,value);
+    }
+
+    @Override
+    public void setRef(int parameterIndex, Ref x) throws SQLException {
+        throw new SQLFeatureNotSupportedException("setRef");
+    }
+
+    @Override
+    public void setSQLXML(int parameterIndex, SQLXML xmlObject) throws SQLException {
+        throw new SQLFeatureNotSupportedException("setSQLXML");
+    }
+
+    @Override
+    public void setArray(int parameterIndex, Array x) throws SQLException {
+        throw new SQLFeatureNotSupportedException("setArray");
+    }
+
+    @Override
+    public void setRowId(int parameterIndex, RowId x) throws SQLException {
+        throw new SQLFeatureNotSupportedException("setRowId");
+    }
+
+    @Override
+    public void setNClob(int parameterIndex, NClob value) throws SQLException {
+        throw new SQLFeatureNotSupportedException("setNClob");
+    }
+
+    @Override
+    public void setNClob(int parameterIndex, Reader reader, long length) throws SQLException {
+        throw new SQLFeatureNotSupportedException("setNClob");
+    }
+
+    @Override
+    public void setNClob(int parameterIndex, Reader reader) throws SQLException {
+        throw new SQLFeatureNotSupportedException("setNClob");
     }
 
     //Set stream object
