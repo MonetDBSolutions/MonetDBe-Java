7c7e104ebaf2d588e2bfb57b0aafb1b55b901d27
bernardom29 <bernardo.mota@monetdbsolutions.com>
2021-04-01 14:42:38 +0100

    Fixing windows floats

--- native/jni/monetdbe._lowlevel.c
+++ native/jni/monetdbe._lowlevel.c
@@ -462,7 +462,7 @@ JNIEXPORT jobjectArray JNICALL Java_org_monetdb_monetdbe_MonetNative_monetdbe_1r
                         c_float->data[i] = 0;
                     }
                 }
-                printf("Float with type " + monetdbe_float + " and " + c_float->count +  " rows\n");
+                printf("Float with type %d and %d rows\n", monetdbe_float, c_float->count);
                 fflush(stdout);
                 addColumnConst(env, j_columns, c_float->data, c_float->name, monetdbe_float, 32 * c_float->count, i, 0);
                 break;
