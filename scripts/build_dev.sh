cb146884c8f1e1a43e00f2c48753d8aac9eab6ef
Bernardo Mota <bernardo@monetdb-nuc3.mdbs.com>
2021-04-22 13:06:26 +0200

    Small build scripts

---
+++ scripts/build_dev.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+    echo "No MonetDB directory instalation provided"
+    exit 1
+fi
+
+cd native
+mvn clean install -DMonetDB_dir=$1
+cd ../java
+mvn clean install
