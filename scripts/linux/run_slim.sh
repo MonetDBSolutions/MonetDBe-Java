a6c6aecbec289558d63acd62bddc79f80a39ec75
bernardom29 <bernardo.mota@monetdbsolutions.com>
2021-04-26 16:14:42 +0200

    Cleanup of the hacky methods used for building the slim jar, giving up on only using maven + scripts

--- scripts/linux/run_slim.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rm -rf /tmp/lib*
-javac -cp java/target/monetdbe-java-1.0-SNAPSHOT-linux-slim.jar example/SimpleTypes.java
-java -cp java/target/monetdbe-java-1.0-SNAPSHOT-linux-slim.jar:example/ SimpleTypes
