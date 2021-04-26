7edb3bd57fd0f8df6c378a2bbcb6dc2c68c3f7a3
bernardom29 <bernardo.mota@monetdbsolutions.com>
2021-04-26 12:01:49 +0200

    More workflow fixes

--- scripts/mac/build_slim.sh
+++ scripts/mac/build_slim.sh
@@ -12,6 +12,11 @@ mvn clean install -DMonetDB_dir=$1 -P mac-release
 mkdir $1/lib-major/
 cp $1/lib/libbat.??.dylib $1/lib/libmapi.??.dylib $1/lib/libmonetdb5.??.dylib $1/lib/libmonetdbe.?.dylib $1/lib/libmonetdbsql.??.dylib $1/lib/libstream.??.dylib $1/lib-major/
 
+cd $1/lib-major/
+for file in *; do install_name_tool -delete_rpath $1/lib $file; done
+for file in *; do install_name_tool -delete_rpath $1/lib/monetdb5 $file; done
+for file in *; do install_name_tool -add_rpath @loader_path/. $file; done
+
 cd ../java
 mvn clean install -DMonetDB_dir=$1 -P mac-slim
 
