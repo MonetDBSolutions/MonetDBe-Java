65950ec0afe3cefa7d3b1a49ae215c1044170e31
bernardom29 <bernardo.mota@monetdbsolutions.com>
2021-04-26 12:34:59 +0200

    Linux slimjar build workflow

--- scripts/linux/build_slim.sh
+++ scripts/linux/build_slim.sh
@@ -5,12 +5,19 @@ if [ $# -eq 0 ]; then
     exit 1
 fi
 
+echo 'Compiling native'
 cd native
 mvn clean install -DMonetDB_dir=$1 -P linux-release
 
 #REALLY BAD CODE BELOW, REMOVE
+echo 'Copying libs'
 mkdir $1/lib-major/
-cp $1/lib64/libbat.so.?? $1/lib64/libmapi.so.?? $1/lib64/libmonetdb5.so.?? $1/lib64/libmonetdbe.so.? $1/lib64/libmonetdbsql.so.?? $1/lib64/libstream.so.?? $1/lib-major/
+cp $1/lib/libbat.so.?? $1/lib/libmapi.so.?? $1/lib/libmonetdb5.so.?? $1/lib/libmonetdbe.so.? $1/lib/libmonetdbsql.so.?? $1/lib/libstream.so.?? $1/lib-major/
+
+echo 'Changing rpath'
+cd $1/lib-major/
+for file in *; do chrpath -r '$ORIGIN/.' $file; done
+cd -
 
 cd ../java
 mvn clean install -DMonetDB_dir=$1 -P linux-slim
--- scripts/mac/build_slim.sh
+++ scripts/mac/build_slim.sh
@@ -5,18 +5,23 @@ if [ $# -eq 0 ]; then
     exit 1
 fi
 
+echo 'Compiling native'
 cd native
 mvn clean install -DMonetDB_dir=$1 -P mac-release
 
 #REALLY BAD CODE BELOW, REMOVE
+echo 'Copying libs'
 mkdir $1/lib-major/
 cp $1/lib/libbat.??.dylib $1/lib/libmapi.??.dylib $1/lib/libmonetdb5.??.dylib $1/lib/libmonetdbe.?.dylib $1/lib/libmonetdbsql.??.dylib $1/lib/libstream.??.dylib $1/lib-major/
 
+echo 'Changing rpath'
 cd $1/lib-major/
 for file in *; do install_name_tool -delete_rpath $1/lib $file; done
 for file in *; do install_name_tool -delete_rpath $1/lib/monetdb5 $file; done
 for file in *; do install_name_tool -add_rpath @loader_path/. $file; done
+cd -
 
+echo 'Compiling jar'
 cd ../java
 mvn clean install -DMonetDB_dir=$1 -P mac-slim
 
