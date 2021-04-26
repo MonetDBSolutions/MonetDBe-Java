343edb8df61cc5afa098b161a00a0724421aca45
bernardom29 <bernardo.mota@monetdbsolutions.com>
2021-04-26 10:56:42 +0200

    Mac slimjar build + fixes to other builds + publish windows artifact

--- scripts/mac/change_rpath.sh
+++ scripts/mac/change_rpath.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 if [ $# -eq 0 ]; then
-    echo "No MonetDB directory instalation provided"
+    echo "No MonetDB lib directory provided"
     exit 1
 fi
 
 cd $1
-for file in ./*.dylib; do install_name_tool -delete_rpath $1 $file; done
-for file in ./*.dylib; do install_name_tool -delete_rpath $1/monetdb5 $file; done
-for file in ./*.dylib; do install_name_tool -add_rpath @loader_path/. $file; done
+for file in *; do install_name_tool -delete_rpath $1 $file; done
+for file in *; do install_name_tool -delete_rpath $1/monetdb5 $file; done
+for file in *; do install_name_tool -add_rpath @loader_path/. $file; done
--- scripts/mac/run_slim.sh
+++ scripts/mac/run_slim.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-rm -rf /var/folders/ps/_wtg4_610zg6d_8n2tp7yj540000gn/T/lib*
+#rm -rf /var/folders/ps/_wtg4_610zg6d_8n2tp7yj540000gn/T/lib*
 javac -cp java/target/monetdbe-java-1.0-SNAPSHOT-mac-slim.jar example/SimpleTypes.java
 java -cp java/target/monetdbe-java-1.0-SNAPSHOT-mac-slim.jar:example/ SimpleTypes
