a6edf6a2e3d29f8aaaa5e04b8393e263847106b0
bernardom29 <bernardo.mota@monetdbsolutions.com>
2021-04-26 14:23:11 +0200

    Trying to get mac slim jar to work

--- scripts/mac/build_slim.sh
+++ scripts/mac/build_slim.sh
@@ -10,11 +10,15 @@ cd native
 mvn clean install -DMonetDB_dir=$1 -P mac-release
 
 #REALLY BAD CODE BELOW, REMOVE
-echo 'Copying libs'
+echo 'Changing rpath for dependencies'
+install_name_tool -change $1/lib/libmonetdbe.1.dylib @rpath/libmonetdbe.1.dylib target/libmonetdbe-java.dylib
+otool -l target/libmonetdbe-java.dylib
+
+echo 'Copying dependency libs to temp folder'
 mkdir $1/lib-major/
 cp $1/lib/libbat.??.dylib $1/lib/libmapi.??.dylib $1/lib/libmonetdb5.??.dylib $1/lib/libmonetdbe.?.dylib $1/lib/libmonetdbsql.??.dylib $1/lib/libstream.??.dylib $1/lib-major/
 
-echo 'Changing rpath'
+echo 'Changing rpath for dependencies'
 cd $1/lib-major/
 for file in *; do install_name_tool -delete_rpath $1/lib $file; done
 for file in *; do install_name_tool -delete_rpath $1/lib/monetdb5 $file; done
