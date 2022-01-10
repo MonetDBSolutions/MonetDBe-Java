ea1835c3f826faec5ad88a1255a18a80162cf9f9
bernardom29 <bernardo.mota@monetdbsolutions.com>
2022-01-10 10:10:40 +0000

    Workflow fixes

--- run_dev.sh
+++ run_dev.sh
@@ -5,5 +5,5 @@ if [ $# -eq 0 ]; then
     exit 1
 fi
 
-javac -cp java/target/monetdbe-java-1.10.jar example/$1.java 
-java -cp java/target/monetdbe-java-1.10.jar:example/ $1
+javac -cp java/target/monetdbe-java-1.11-SNAPSHOT.jar example/$1.java 
+java -cp java/target/monetdbe-java-1.11-SNAPSHOT.jar:example/ $1
